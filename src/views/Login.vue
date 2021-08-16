3133fd5e9ba9f75e14079700470c9b0053c995b9
heikousens <anacmp3711@gmail.com>
2021-08-16 17:04:58 -0300

    Update Login.vue
    Inserindo validação de e-mail e senha no Firebase para a página de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,15 +4,16 @@
             <div class="container-fluid">
                 <div class="container">
                     <div class="row">
-                        <form class="col-8 mx-auto">
+                        <div v-if="error" class="alert alert-danger">{{error}}</div>
+                        <form class="col-8 mx-auto" action="" @submit.prevent="submit" method="POST">
                             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
         
                             <div class="form-floating">
-                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" v-model="form.email">
                                 <label for="floatingInput">Email address</label>
                             </div>
                             <div class="form-floating">
-                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" v-model="form.password">
                                 <label for="floatingPassword">Password</label>
                             </div>
         
@@ -32,10 +33,34 @@
 </template>
 
 <script>
+import firebase from "firebase";
 
 export default {
   name: 'Login',
   components: {
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      },
+      error: null
+    };
+  },
+  methods: {
+    submit() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.form.email, this.form.password)
+        .then(data => {
+            console.log(data);
+          this.$router.replace({ name: "Home" });
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    }
   }
 }
 </script>
