c687974c351685eafb54633c2b1d2bb4f518c2cc
heikousens <anacmp3711@gmail.com>
2021-08-16 00:26:37 -0300

    Firebase auth
    Incluindo autenticação de novo usuário no site, armazenando informações no Firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
+import firebase from "firebase/app";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,10 +10,21 @@ import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
 import './components/style.css';
 import router from './router'
 
+Vue.config.productionTip = false;
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
-Vue.config.productionTip = false
+const firebaseConfig = {
+  apiKey: "AIzaSyDigjYrc1x-aYIa0cYTKPZwosEZyIV75N0",
+  authDomain: "pokemon-lovers-cc3d7.firebaseapp.com",
+  projectId: "pokemon-lovers-cc3d7",
+  storageBucket: "pokemon-lovers-cc3d7.appspot.com",
+  messagingSenderId: "130647182285",
+  appId: "1:130647182285:web:b591f67d75efcfad169284"
+}
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,26 +3,26 @@
         <div class="container-fluid">
             <div class="container">
                 <div class="row">
-                    <form class="col-8 mx-auto">
+                    <form class="col-8 mx-auto" action="" @submit.prevent="submit" method="POST">
                         <h1 class="h3 mb-3 fw-normal">Fill the information below to sign up</h1>
                             <div class="form-floating" style="margin-top: 3%;">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="Your name">
-                                <label for="floatingInput">Name</label>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Your name" value required autofocus v-model="form.name">
+                                <label for="email">Name</label>
                             </div>
                             <div class="form-floating" style="margin-top: 3%;">
-                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" value required autofocus v-model="form.email">
                                 <label for="floatingInput">Email address</label>
                             </div>
                             <div class="form-floating" style="margin-top: 3%;">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="Your Username">
+                                <input type="text" class="form-control" id="username" placeholder="Your Username" required autofocus v-model="form.username">
                                 <label for="floatingInput">Username</label>
                             </div>
                             <div class="form-floating" style="margin-top: 3%;">
-                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <input type="password" class="form-control" id="password" placeholder="Password" required autofocus v-model="form.password">
                                 <label for="floatingPassword">Password</label>
                             </div>
                             <div class="row" style="margin-top: 5%;">
-                                <button @click="$router.push('quiz-info')" class="w-50 btn btn-lg poke-secondary" type="submit">Next</button>
+                                <button class="w-50 btn btn-lg poke-secondary" type="submit">Next</button>
                                 <button class="w-50 btn btn-lg poke-primary" type="reset">Clear</button>
                             </div>
                     </form>
@@ -34,10 +34,41 @@
 </template>
 
 <script>
+import firebase from "firebase";
 
 export default {
   name: 'Sign-Up',
   components: {
+  },
+  data() {
+    return {
+      form: {
+        errors: [],
+        name: "",
+        email: "",
+        username: "",
+        password: ""
+      },
+      error: null
+    };
+  },
+  methods: {
+    submit() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.form.email, this.form.password)
+        .then(data => {
+          data.user
+            .updateProfile({
+              displayName: this.form.name
+            })
+            /*.then(() => {});*/
+            .then(this.$router.replace({ name: "PreTest" }));
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    }
   }
 }
 </script>
