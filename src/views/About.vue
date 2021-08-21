440fbca5addd1af6737dcb923d11e296e65a292b
Jennifer Omena <jennifer.omena@outlook.com>
2021-08-20 22:06:36 -0300

    Update About Page
    Ainda falta modificar muitas coisas

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-section">
+      <h1>About Us</h1>
+    </div>
+    <h2 style="text-align:center">Our Team</h2>
+    <div class="row">
+      <div class="column">
+        <div class="card">
+          <img src="https://i.pinimg.com/originals/f5/1d/08/f51d08be05919290355ac004cdd5c2d6.png" alt="Ana Carolina" style="width:30%">
+          <div class="container">
+            <h2>Ana Carolina</h2>
+            <p class="title">CEO & Founder</p>
+            <p>anacarolina@email.com</p>
+            <p><button class="button">Github</button></p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="https://i.pinimg.com/originals/e7/0e/d0/e70ed0b54f9230c56c2e3bd2958d68a4.jpg" alt="Bruno Cister" style="width:30%">
+          <div class="container">
+            <h2>Bruno Cister</h2>
+            <p class="title">CEO & Founder</p>
+            <p>brunocister@email.com</p>
+            <p><button class="button">Github</button></p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="https://http2.mlstatic.com/D_NQ_NP_798039-MLB44296213240_122020-O.jpg" alt="Jennifer Omena" style="width:30%">
+          <div class="container">
+            <h2>Jennifer Omena</h2>
+            <p class="title">CEO & Founder</p>
+            <p>jenniferomena@email.com</p>
+            <p><button class="button">Github</button></p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="https://i.pinimg.com/originals/af/7c/19/af7c1907e3d2e5f142b81ad6a667fd5c.jpg" alt="Wallace dos Anjos" style="width:30%">
+          <div class="container">
+            <h2>Wallace dos Anjos</h2>
+            <p class="title">CEO & Founder</p>
+            <p>wallacedosanjos@email.com</p>
+            <p><button class="button">Contact</button></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
